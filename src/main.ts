16eecae1d4779b4378aa60ec5fd30ed92753ae2f
Shubham Chaudhary <shubhamchaudhary1399@gmail.com>
2022-06-20 16:16:27 +0530

    changes some old files

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-
 // console.log(process.env.NODE_ENV);
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
